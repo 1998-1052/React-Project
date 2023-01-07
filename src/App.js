e410e8047e860d77feada39902c75e9c89f52c58
avanish <avanish.p@kftpl.com>
2023-01-07 19:28:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState } from "react";
+import Header from "./Cmponents/Layout/Header";
+import "./App.css";
+// import { Fragment } from "react";
+import Meals from "./Cmponents/Meals/Meals";
+import Cart from "./Cmponents/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsShow, setCartIsShow] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShow(true);
+  };
+  const hideCardHandler = () => {
+    setCartIsShow(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div>
+    <CartProvider>
+      {/* // <Fragment> */}
+      {cartIsShow && <Cart onClose={hideCardHandler} />}
+
+      {/* <Cart /> */}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+      {/* </div> */}
+      {/* </Fragment> */}
+    </CartProvider>
   );
 }
 
